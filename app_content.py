28e514aea6b0c646e36c2cbb582d82c29e403ec2
bbe088 <benjamin.beuster@nsd.no>
2023-10-21 00:39:17 +0200

    active row with first callback, return variable name

---
+++ app_content.py
@@ -0,0 +1,49 @@
+# Modified html_text to use markdown
+markdown_text = """
+## DDI-CDI Subset
+
+This profile utilizes 20 classes from the DDI-CDI model and 2 classes from the SKOS model:
+
+| DDI-CDI Model                  | SKOS Model          |
+|--------------------------------|---------------------|
+| DataStore                      | skos:ConceptScheme  |
+| PhysicalDataset                | skos:Concept        |
+| PhysicalRecordSegment          |                     |
+| PhysicalSegmentLayout          |                     |
+| ValueMapping                   |                     |
+| ValueMappingPosition           |                     |
+| DataPoint                      |                     |
+| DataPointPosition              |                     |
+| InstanceValue                  |                     |
+| LogicalRecord                  |                     |
+| WideDataSet                    |                     |
+| WideDataStructure              |                     |
+| IdentifierComponent            |                     |
+| MeasureComponent               |                     |
+| PrimaryKey                     |                     |
+| PrimaryKeyComponent            |                     |
+| InstanceVariable               |                     |
+| SubstantiveConceptualDomain    |                     |
+| SentinelConceptualDomain       |                     |
+| ValueAndConceptDescription     |                     |
+"""
+
+colors = {'background': '#111111', 'text': '#7FDBFF'}
+
+style_dict = {
+    'backgroundColor': colors['background'],
+    'textAlign': 'center',
+    'color': 'white',
+    'fontSize': 14,
+}
+
+header_dict = {
+    'backgroundColor': colors['background'],
+    'textAlign': 'center',
+    'color': colors['text'],
+    'fontSize': 16,
+}
+
+
+table_style = {'overflowX': 'auto', 'overflowY': 'auto', 'maxHeight': '350px',
+               'maxWidth': 'auto', 'marginTop': '10px'}
